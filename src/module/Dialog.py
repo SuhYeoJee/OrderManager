9be5e5ad1f3646d2d7f8c4a2713a9d528781cc40
SuhYeoJee <72551449+SuhYeoJee@users.noreply.github.com>
2025-02-24 14:35:01 +0900

    본드위젯읽기쓰기

--- src/module/Dialog.py
+++ src/module/Dialog.py
@@ -50,8 +50,41 @@ class BaseDialog(BaseUI):
    
 # ===========================================================================================
 # 클래스 자동생성
+TABLES.remove('bond')
 for table_name in TABLES:
     class_name = f'{table_name.capitalize()}Dialog'
     globals()[class_name] = type(class_name, (BaseDialog,), {
         '__init__': lambda self, dialog_type, table_name, parent=None: super(type(self), self).__init__(dialog_type, table_name, parent)
     })
+
+
+class BondDialog(BaseDialog):
+    def __init__(self, dialog_type, table_name, parent=None):
+        super().__init__(dialog_type, table_name, parent)
+        self._set_inner_widget()
+        self.input_widgets = self.get_input_widgets()
+        self.set_powder_combobox()
+
+    def _set_inner_widget(self):
+        self.bondInnerWidget = QWidget()
+        self.scrollLayout = QVBoxLayout(self.scrollAreaWidgetContents)
+        loadUi("./ui/bondInnerWidget.ui", self.bondInnerWidget)
+        self.scrollLayout.addWidget(self.bondInnerWidget)
+
+    def _set_powder_combobox(self):
+        powders = [' ',"co","fe","cu_300","cu_600","ni","cusn_67","cusn_80","w","wc","w2c","s","ag","zn"]
+        for i in range(1,6):
+            combo_box = self.bondInnerWidget.findChild(QComboBox, f'powder_{i}ComboBox')
+            combo_box.clear()
+            combo_box.addItems(powders)
+        combo_box = self.bondInnerWidget.findChild(QComboBox, f'powder_6ComboBox')
+        combo_box.clear()
+        combo_box.addItems([' ','p2o5'])
+
+    def set_fks(self,datas):
+        super().set_fks(datas)
+        self._set_powder_combobox()
+
+
+    # 시그널 박아서 밀도 가져오기 
+    
